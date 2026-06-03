a978c38f4ca822e49a3a047c17d7415eb5f5e981
Bilal Shaikh <bilalshaikhh339@gmail.com>
2026-06-03 20:11:14 +0500

    i

--- app.js
+++ app.js
@@ -180,3 +180,10 @@ function PercentageSol(){
 
   
 // }
+
+const glow = document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove",(e)=>{
+  glow.style.left = e.clientX + "px";
+  glow.style.top = e.clientY + "px";
+});
